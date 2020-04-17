d72f17b2582b5ddb47a4babf2182d04ec28f5cbe
Igor Couto <igor.fcouto@gmail.com>
2020-04-17 11:27:21 -0300

    Initial Commit

---
+++ 3dModelDisplay/ClientApp/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+            Selec an <code>3d model</code> file and its <code>texture</code> to upload
+        </p>
+        <a
+          className="App-link"
+            href="https://github.com/igor-couto"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source Code
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 3dModelDisplay/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace _3dModelDisplay.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) 
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
