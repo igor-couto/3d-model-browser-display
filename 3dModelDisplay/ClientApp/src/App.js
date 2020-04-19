3527b2437ea35d59a9185e08d478553c93b38ae4
Igor Couto <igor.fcouto@gmail.com>
2020-04-18 23:49:57 -0300

    Header and Drop Zone components

--- 3dModelDisplay/ClientApp/src/App.js
+++ 3dModelDisplay/ClientApp/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import DropZone from './components/DropZone/DropZone';
 import Display from './components/Display/Display';
 
 function App() {
@@ -9,15 +11,16 @@ function App() {
       <header className="App-header">
         <p>
           <label htmlFor="input-model">Select a <code>3d model</code> file</label>
-            <input className="InputModelUpload" id="input-model" type="file"></input>
+            <input className="InputModelUpload" id="input-model" type="file" multiple></input>
         </p>
         <p>
             Select a <code>Texture</code> file
-            <input className="InputTextureUpload" type="file"></input>
+            <input className="InputTextureUpload" type="file" multiple></input>
         </p>
 
       </header>
-      <Display/>
+      <Header/>
+      <DropZone onFilesAdded={console.log}/>
       <Footer/>
     </div>
   );
