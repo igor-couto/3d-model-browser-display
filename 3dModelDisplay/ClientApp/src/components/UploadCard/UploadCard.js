04a750487df3afb0f8491f86d5483bf42f1468d2
Igor Couto <igor.fcouto@gmail.com>
2020-04-21 03:09:09 -0300

    Update Upload Card

--- 3dModelDisplay/ClientApp/src/components/UploadCard/UploadCard.js
+++ 3dModelDisplay/ClientApp/src/components/UploadCard/UploadCard.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import DropZone from '../DropZone/DropZone';
 import styles from './UploadCard.module.css';
+import {ReactComponent as CloseIcon} from './close-icon.svg';
 
 export default class UploadCard extends Component {
 
@@ -12,17 +13,20 @@ export default class UploadCard extends Component {
 
     }
 
+    CloseCard = () => {
+        this.props.openNewModelCard(false);
+    }
+
     render() {
         if(this.props.isNewModelCardOpen){
             return(
                 <div className={styles.cardContainer}>
                     <div className={styles.card}>
+                        <CloseIcon className={styles.close} onClick={this.CloseCard}/>
                         <DropZone onFilesAdded={console.log}/>
                         <input className={styles.textInput} placeholder="Model Name ..."/>
                         <button className={styles.button} onClick={this.UploadModel}>Upload</button>
-    
                     </div>
-                    
                 </div>
             );
         }else {
