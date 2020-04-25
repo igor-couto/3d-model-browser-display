c030a4a305598491e4d7bd76c2e8e91001a551f5
Igor Couto <igor.fcouto@gmail.com>
2020-04-24 22:19:38 -0300

    Create GetModel and GetTexture actions

--- 3dModelDisplay/Controllers/ModelController.cs
+++ 3dModelDisplay/Controllers/ModelController.cs
@@ -1,6 +1,7 @@
 ﻿using _3dModelDisplay.Model;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -8,6 +9,60 @@ namespace _3dModelDisplay.Controllers
 {
     public class ModelController : Controller
     {
+        public IActionResult GetModel (string modelName)
+        {
+            try 
+            {
+                var path = @$"C:\temp/{modelName}/{modelName}.obj";
+
+                var mimeType = "application/octet-stream";
+                var fileBytes = OpenFile(path);
+
+                return new FileContentResult(fileBytes, mimeType)
+                {
+                    FileDownloadName = modelName
+                };
+            }
+            catch(Exception exception)
+            {
+                return new NotFoundResult();
+            }
+        }
+
+        public IActionResult GetTexture(string modelName)
+        {
+            try
+            {
+                var path = @$"C:\temp/{modelName}/{modelName}.png";
+
+                var mimeType = "image/png";
+                var fileBytes = OpenFile(path);
+
+                return new FileContentResult(fileBytes, mimeType)
+                {
+                    FileDownloadName = modelName
+                };
+            }
+            catch (Exception exception)
+            {
+                return new NotFoundResult();
+            }
+        }
+
+        private byte[] OpenFile(string filename) 
+        {
+            using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                // Create a byte array of file stream length
+                byte[] bytes = System.IO.File.ReadAllBytes(filename);
+                //Read block of bytes from stream into the byte array
+                fs.Read(bytes, 0, System.Convert.ToInt32(fs.Length));
+                //Close the File Stream
+                fs.Close();
+                return bytes; //return the byte data
+            }
+        }
+
         [HttpGet]
         public IActionResult GetAll()
         {
