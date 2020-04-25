2762a76ff1a134459d419943401a4bbb0f1accb8
Igor Couto <igor.fcouto@gmail.com>
2020-04-24 22:23:23 -0300

    Menu click functionality: fetch 3D model from server

--- 3dModelDisplay/ClientApp/src/components/ModelList/ModelList.js
+++ 3dModelDisplay/ClientApp/src/components/ModelList/ModelList.js
@@ -5,10 +5,10 @@ export default class ModelList extends Component {
     
     constructor(props){
         super(props);
-    }
-
-    state = {
-        models: []
+        
+        this.state = {
+            models: []
+        }
     }
 
     componentDidMount(){
@@ -16,24 +16,41 @@ export default class ModelList extends Component {
     }
 
     loadModels = async () => {
-        const response = await fetch('https://localhost:44355/Model/GetAll', {method: 'GET'});
+        const response = await fetch('https://localhost:44355/Model/GetAll');
         const responseModels = await response.json();
 
         this.setState({
             models: responseModels
         });
+
+        if(this.state.models.lenght > 0)
+            this.setState( { selectedModelIndex: 0 } );
     };
 
     OpenUploadCard = () => {
         this.props.openNewModelCard(true);
+    }
+
+    SelectModel = async (index) => {
+        this.setState({selectedListItem: index});
+        
+        let urlGetModel = `https://localhost:44355/Model/GetModel?modelName=${this.state.models[index].Name}`;
+        const responseModel = await fetch(urlGetModel);
+
+        let urlGetTexture = `https://localhost:44355/Model/GetTexture?modelName=${this.state.models[index].Name}`;
+        const responseTexture = await fetch(urlGetTexture);
     }
     
     render(){
         return(
             <div className={styles.leftContainer}>
                 <div className={styles.list}>
-                    {this.state.models.map( model => (
-                        <span key={model.Name} className={styles.listItem}>
+                    {this.state.models.map( (model, index) => (
+                        <span 
+                            key={model.Name} 
+                            className={ `${styles.listItem} ${(index === this.state.selectedModelIndex)? styles.selectedListItem : ''}`}
+                            onClick={   (e) => { this.SelectModel(index) }  }
+                        >
                             {model.Name}
                         </span>
                     ))}
--- 3dModelDisplay/ClientApp/src/components/UploadCard/UploadCard.js
+++ 3dModelDisplay/ClientApp/src/components/UploadCard/UploadCard.js
@@ -10,7 +10,7 @@ export default class UploadCard extends Component {
     }
 
     UploadModel = () => {
-
+        
     }
 
     CloseCard = () => {
