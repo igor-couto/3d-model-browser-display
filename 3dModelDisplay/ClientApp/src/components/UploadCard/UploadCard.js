7eec9525ebc00210600167fec45a787156c6c3c6
Igor Couto <igor.fcouto@gmail.com>
2020-04-25 20:48:35 -0300

    Remove unecessary constructor from UploadCard

--- 3dModelDisplay/ClientApp/src/components/UploadCard/UploadCard.js
+++ 3dModelDisplay/ClientApp/src/components/UploadCard/UploadCard.js
@@ -5,10 +5,6 @@ import {ReactComponent as CloseIcon} from './close-icon.svg';
 
 export default class UploadCard extends Component {
 
-    constructor(props){
-        super(props);
-    }
-
     UploadModel = () => {
         
     }
