258fc9a029aeca6a9ece0b855227f3cebda117f1
Igor Couto <igor.fcouto@gmail.com>
2020-04-30 22:57:19 -0300

    Fix 3D rendering bug

--- 3dModelDisplay/ClientApp/src/components/ModelDisplay/ModelDisplay.js
+++ 3dModelDisplay/ClientApp/src/components/ModelDisplay/ModelDisplay.js
@@ -1,69 +1,93 @@
 import React, { Component } from 'react';
 import styles from './ModelDisplay.module.css'
 import * as THREE from './three.module';
+//import * as LOADER from './OBJLoader';
 
 export default class ModelDisplay extends Component {
 
-    componentDidMount(){
+    componentDidUpdate() {
 
-        // const script = document.createElement("script");
-        // script.src = "./three.js";
-        // script.async = true;
-        // document.body.appendChild(script);
-
-        // const script2 = document.createElement("script");
-        // script2.src = "./three.min.js";
-        // script2.async = true;
-        // document.body.appendChild(script2);
-
-        // const script3 = document.createElement("script");
-        // script3.src = "./loaders/OBJLoader.js";
-        // script3.async = true;
-        // document.body.appendChild(script3);
-
-        this.loadScene();
+        if(this.props.model !== undefined){
+            this.loadScene();
+        }
     }
 
     loadScene = () => {
 
-
         var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 1, 2000 );
-        var renderer = new THREE.WebGLRenderer();
+        var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 1, 1000 );
+        var renderer = new THREE.WebGLRenderer({antialias: true});
         renderer.setPixelRatio( window.devicePixelRatio );
-        renderer.setSize( window.innerWidth/4, window.innerHeight/4 );
-        document.getElementById('3dContainer').appendChild(renderer.domElement);
+        renderer.setClearColor('#171717');
+
+        // TODO: delete webgl context. The dom element is being erased but the context didn't
+        let fatherElement = document.getElementById('3dContainer');
+        renderer.setSize( fatherElement.offsetWidth, fatherElement.offsetHeight );
+        if(fatherElement.childNodes[0] !== undefined){
+            fatherElement.removeChild(fatherElement.childNodes[0]);
+        }
+        fatherElement.appendChild(renderer.domElement);
 
         const ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
         scene.add( ambientLight );
         const pointLight = new THREE.PointLight( 0xffffff, 0.8 );
         camera.add( pointLight );
 
+        scene.add( camera );
+
+        var material = new THREE.MeshPhongMaterial( { color: 0x829FBE } ); 
+        var loader = new THREE.ObjectLoader();
+
+
+        //var loader2 = new LOADER.OBJLoader();
+        //console.log(this.props.model);
+        //var jsonObject = JSON.stringify( loader.parse( this.props.model ).toJSON(), this.parseNumber );
+
+        
+        const manager = new THREE.LoadingManager();
+        manager.onProgress = function ( item, loaded, total ) {
+            console.log( item, loaded, total );
+        };
+        const textureLoader = new THREE.TextureLoader( manager );
+        const texture = textureLoader.load( this.props.texture );
+
+        // const onProgress = function ( xhr ) {
+        //     if ( xhr.lengthComputable ) {
+        //         let percentComplete = xhr.loaded / xhr.total * 100;
+        //         console.log( Math.round(percentComplete, 2) + '% downloaded' );
+        //     }
+        // };
+        // const onError = function ( xhr ) {
+        // };
+
+        // loader.load ( this.props.model, function ( model ) {
+        //     let object = model;
+        //     object.traverse( function ( child ) {
+        //         // if ( child instanceof THREE.Mesh ) {
+        //         //     child.material.map = texture;
+        //         // }
+        //         if ( child instanceof THREE.Mesh ) {
+        //             child.material = material;
+        //         }
+        //     } );
+        //     object.position.x = 0;
+        //     object.position.y = 0;
+        //     object.position.z = 0;
+        //     scene.add( object );
+        // }, onProgress, onError );
+
         var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        var material = new THREE.MeshBasicMaterial( { color: 0x00ee00 } );
         var cube = new THREE.Mesh( geometry, material );
         scene.add( cube );
-        camera.position.z = 50;
+        camera.position.z = 6;
+        camera.position.y = 1
+        camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
         var animate = function () {
-          requestAnimationFrame( animate );
-          cube.rotation.x += 0.01;
-          cube.rotation.y += 0.01;
-          renderer.render( scene, camera );
+            requestAnimationFrame( animate );
+            cube.rotation.y += 0.01;
+            renderer.render( scene, camera );
         };
         animate();
-
-
-        // const container = document.getElementById('3dContainer')
-        // document.body.appendChild( container );
-        // const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-        // camera.position.z = 250;
-        // // scene
-        // const scene = new THREE.Scene();
-        // const ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
-        // scene.add( ambientLight );
-        // const pointLight = new THREE.PointLight( 0xffffff, 0.8 );
-        // camera.add( pointLight );
-        // scene.add( camera );
         
         // // // texture
         // const manager = new THREE.LoadingManager();
@@ -95,45 +119,18 @@ export default class ModelDisplay extends Component {
         //     scene.add( object );
         // }, onProgress, onError );
     
-        // const renderer = new THREE.WebGLRenderer();
-        // renderer.setPixelRatio( window.devicePixelRatio );
-        // renderer.setSize( window.innerWidth, window.innerHeight );
-        // container.appendChild( renderer.domElement );
-    
-        // window.addEventListener( 'resize', this.onWindowResize, false );
-    
-        // this.setState({
-        //     container = document.getElementById('3dContainer'),
-        //     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 ),
-        //     scene = new THREE.Scene(),
-        //     renderer = new THREE.WebGLRenderer(),
-        //     windowHalfX = window.innerWidth / 2,
-        //     windowHalfY = window.innerHeight / 2,
-        //     rotationspeed = 0.03,
-        //     object = null
-        // });
-
-        //this.renderScene();
-
-        // camera.lookAt( scene.position );
-        // renderer.render( scene, camera );
+        window.addEventListener( 'resize', this.onWindowResize, false );
     };
 
-    // onWindowResize = () => {
-    //     windowHalfX = window.innerWidth / 2;
-    //     windowHalfY = window.innerHeight / 2;
-    //     camera.aspect = window.innerWidth / window.innerHeight;
-    //     camera.updateProjectionMatrix();
-    //     renderer.setSize( window.innerWidth, window.innerHeight );
-    // }
-
-    renderScene() {
-        //requestAnimationFrame( animate );
-        //object.rotateY(rotationspeed);
-        // camera.lookAt( scene.position );
-        // renderer.render( scene, camera );
+    parseNumber ( key, value ) {
+        var PRECISION = 6;
+        return typeof value === 'number' ? parseFloat( value.toFixed( PRECISION ) ) : value;  
     }
 
+    onWindowResize = () => {
+        //TODO: Not ideal. Should resize renderer instead.
+        this.loadScene();
+    }
 
     render() {
         return(
